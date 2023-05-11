59dde37ff0d1c44f4e3718aeac687e2c6e835d5d
Steven <steven.link@outlook.com>
2023-05-11 18:14:05 -0500

    first upload of work to github

---
+++ Assets/JS/questions.js
@@ -0,0 +1,110 @@
+// questions and answers
+var questions = [
+  {
+    title: "What below is a variable keyword?",
+    choices: [
+      "Var",
+      "Let",
+      "Const",
+      "All of the above"],
+    answer: "All of the above"
+  },
+  {
+    title: "What does CSS stand for?",
+    choices: [
+      "Cascading style sheets",
+      "Color and style sheets",
+      "Coded style sheets",
+      "None of the above"
+    ],
+    answer: "Cascading style sheets"
+  },
+  {
+    title: "Which of the following is not a boolean operator:",
+    choices: [
+      "Or",
+      "Not",
+      "And",
+      "All the above are boolean operators"],
+    answer: "All the above are boolean operators"
+  },
+  {
+    title: "What does a reset.css sheet do?",
+    choices: [
+      "Reloads the webpage",
+      "Circumvents inconsistencies across browsers",
+      "Resets variables",
+      "All the above"
+    ],
+    answer: "Circumvents inconsistencies across browsers"
+  },
+  {
+    title: "What are the three different types of errors in JavaScript?",
+    choices: [
+      "Animation time errors, run time errors, and load time errors",
+      "Load time errors, run time errors, and logical errors",
+      "Run time errors, logical errors, and animation time errors",
+      "Logical Errors, animation time errors, and load time errors"
+    ],
+    answer: "Load time errors, run time errors, and logical errors"
+  },
+  {
+    title: "Which of the following is/are the data type(s) of variables in JavaScript?",
+    choices: [
+      "Object data types",
+      "Function data types",
+      "Parent data types",
+      "All of the above"
+    ],
+    answer: "Object data types"
+  },
+  {
+    title:
+      "A very useful tool for testing and debugging for printing content to the debugger is:",
+    choices: [
+      "Repeater",
+      "Command Prompt",
+      "Console.log",
+      "For loops"
+    ],
+    answer: "Console.log"
+  },
+  {
+    title: "What is an if else statement?",
+    choices: [
+      "An arguement to a function",
+      "Conditional statement depending if an expression is true or false",
+      "Data of a variable to answer the call of a function",
+      "None of the above"
+    ],
+    answer: "Conditional statement depending if an expression is true or false"
+  },
+  {
+    title: "What is an array used to store?",
+    choices: [
+      "Booleans",
+      "Numbers and strings",
+      "Other arrays",
+      "All of the above"
+    ],
+    answer: "All of the above"
+  },
+  {
+    title: "When assigned to a variable, string values must be enclosed within what?",
+    choices: [
+      "Tildes",
+      "Curly brackets",
+      "Quotes",
+      "Asterisk"],
+    answer: "Quotes"
+  },
+  {
+    title: "What type of  Pop up boxes are available in JavaScript?:",
+    choices: [
+      "Prompt",
+      "Alert",
+      "Confirm",
+      "All the above"],
+    answer: "All the above"
+  }
+];
